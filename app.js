8364cfbf2036cac0c326ef4cbf7e57f87efe83c9
Fernando Cabrera <ferncabrera01@outlook.com>
2021-01-12 00:55:47 -0500

    re-try deployment

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'))
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', (req, res) => {
+    res.render('index')
+});
+
+app.listen(3000, () => {
+    console.log('Serving on port 3000')
+});
